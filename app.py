66481bf455a86e74ddbaf49bd2823beece4fcf7b
Travis Souther <souther-trav@hotmail.com>
2024-04-30 18:36:51 -0500

    added part 2

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'ihaveasecret'
+app.config['SECRET_KEY'] = 'my_key'
 app.app_context().push()
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
@@ -21,9 +21,16 @@ db.create_all()
 
 @app.route('/')
 def root():
-    """Homepage redirects to list of users."""
+    """Show recent list of posts, most-recent first.""" 
+    posts = Post.query.order_by(Post.created_at.desc()).limit(5).all()
+    return render_template("posts/homepage.html", posts=posts)
 
-    return redirect("/users")
+@app.errorhandler(404)
+def page_not_found(err):
+    """show an error page"""
+
+    
+    return render_template("404.html"), 404
 
 
 ##############################################################################
@@ -85,6 +92,7 @@ def users_update(user_id):
     user.last_name = request.form['last_name']
     user.image_url = request.form['image_url']
     user.power_level = request.form['power_level']
+
     db.session.add(user)
     db.session.commit()
 
@@ -100,3 +108,71 @@ def users_destroy(user_id):
     db.session.commit()
 
     return redirect("/users")
+###################################################################
+
+
+@app.route('/users/<int:user_id>/posts/new')
+def posts_new_form(user_id):
+    """show a form to create a post for a certain user """
+
+    user = User.query.get_or_404(user_id)
+    return render_template('posts/new.html' , user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new' , methods=["POST"])
+def new_posts(user_id):
+    """Handle form submission for creating a new post for a specific user"""
+
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title=request.form['title'],
+                    content=request.form['content'],
+                    user=user)
+    
+    db.session.add(new_post)
+    db.session.commit()
+
+
+    return redirect(f"/users{user_id}")
+
+@app.route('/posts/<int:post_id>')
+def posts_show(post_id):
+    """show a page for a certain post """
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/show.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit')
+def posts_edit(post_id):
+    """Show a form to edit an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/edit.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def posts_update(post_id):
+    """Handle form submission for updating an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+    
+
+    return redirect(f"/users/{post.user_id}")
+
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def posts_destroy(post_id):
+    """Handle form submission for deleting an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+    
+
+    return redirect(f"/users/{post.user_id}")
+
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 """SQLAlchemy models for blogly."""
+from datetime import timezone, datetime
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -17,12 +18,35 @@ class User(db.Model):
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
     power_level = db.Column(db.Integer, nullable=False, default=9000)
+
+    posts = db.relationship('Post', backref="user", cascade="all, delete-orphan")
     @property
     def full_name(self):
         """Return full name of user with power level"""
 
         return f"{self.first_name} {self.last_name} : power level {self.power_level}"
 
+
+
+
+class Post(db.Model):
+    """Model for posts"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.now(timezone.utc))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    @property
+    def formated_date(self):
+        """Returns a well formated date"""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
 
 def connect_db(app):
     """Connect this database to provided Flask app.
